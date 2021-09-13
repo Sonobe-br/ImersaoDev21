a9ee57dfe8e78fe0a05413161c1183f36b0a2180
Daniel Sonobe Silveira <dev.obe78@gmail.com>
2021-09-13 12:19:19 -0300

    Imersão DEV Alura - Class1

---
+++ calculadoraDeMedias.js
@@ -0,0 +1,15 @@
+let nome = "Guilherme"
+let notaDoPrimeiroBimestre = 9;
+let notaDoSegundoBimestre = 7;
+let notaDoTerceiroBimestre = 4;
+let notaDoQuartoBimestre = 2;
+
+var notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre) / 4;
+var notaFixada = notaFinal.toFixed(2);
+  
+console.log("Bem-vindo " + nome)
+console.log(notaFixada)
+
+/* Revisão:
+Aprendemos sobre valores do tipo 'string' e number, também vimos as saídas dos resultados usando console.log 
+com concatenação, toFixed, operações aritméticas */
