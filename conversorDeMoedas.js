bcc3246b80a0d01afb5a5ad184c934a402b6751a
Daniel Sonobe Silveira <dev.obe78@gmail.com>
2021-09-28 23:15:12 -0300

    imersãoDEV - Conversor de Moedas_ajustando códigos

--- conversorDeMoedas.js
+++ conversorDeMoedas.js
@@ -24,6 +24,21 @@ function ConverterReal() {
     console.log(valorEmReal);
 
     var elementoValorConvertido = document.getElementById('valorConvertido');
-    var valorConvertido = 'O resultado em dólar é R$' + valorEmReal;
+    var valorConvertido = 'O resultado em Dólar é R$' + valorEmReal;
+    elementoValorConvertido.innerHTML = valorConvertido;   
+}
+
+function ConverterDolar() {
+    var elementoValor = document.getElementById('valor');
+    var valor = elementoValor.value;
+    var valorEmDolarNumerico = parseInt(valor); 
+    //console.log(valorEmRealNumerico);
+    
+    //conversão de valores em dólar para real
+    var valorEmDolar = valorEmDolarNumerico.toFixed(1) / 0.18; /* Cotação do dia 14 de setembro 2021 */
+    console.log(valorEmDolar);
+
+    var elementoValorConvertido = document.getElementById('valorConvertido');
+    var valorConvertido = 'O resultado em Real é R$' + valorEmDolar;
     elementoValorConvertido.innerHTML = valorConvertido;   
 }
