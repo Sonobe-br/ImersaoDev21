5ae615cf99bd6f1cdad73653d7161858a0ad82cf
Daniel Sonobe Silveira <dev.obe78@gmail.com>
2021-09-29 00:55:12 -0300

    ImersãoDev - Arrumando o javaScript e fazendo o programa rodar 100%

--- conversorDeMoedas.js
+++ conversorDeMoedas.js
@@ -1,44 +1,27 @@
 function Converter() {
     var valorElemento = document.getElementById('valor');
     var valor = valorElemento.value;
-    var valorEmDolarNumerico = parseInt(valor); 
+    var valorEmRealNumerico = parseInt(valor); 
     //console.log(valorEmDolarNumerico);
     
-    //conversão de valores em dólar para real
-    var valorEmDolar = valorEmDolarNumerico.toFixed(2) / 5.24; /* Cotação do dia 14 de setembro 2021 */
-    console.log(valorEmDolar);
-
-    var elementoValorConvertido = document.getElementById('valorConvertido');
-    var valorConvertido = 'O resultado em real é R$' + valorEmDolar;
-    elementoValorConvertido.innerHTML = valorConvertido;   
-}
-
-function ConverterReal() {
-    var valorElemento = document.getElementById('valor');
-    var valor = valorElemento.value;
-    var valorEmRealNumerico = parseInt(valor); 
-    //console.log(valorEmRealNumerico);
-    
-    //conversão de valores em dólar para real
-    var valorEmReal = valorEmRealNumerico.toFixed(2) / 5.24; /* Cotação do dia 14 de setembro 2021 */
+    var valorEmReal = valorEmRealNumerico.toFixed(2) / 5.00; /* Cotação do dia 14 de setembro 2021 */
     console.log(valorEmReal);
-
-    var elementoValorConvertido = document.getElementById('valorConvertido');
-    var valorConvertido = 'O resultado em Dólar é R$' + valorEmReal;
-    elementoValorConvertido.innerHTML = valorConvertido;   
-}
-
-function ConverterDolar() {
-    var elementoValor = document.getElementById('valor');
-    var valor = elementoValor.value;
-    var valorEmDolarNumerico = parseInt(valor); 
-    //console.log(valorEmRealNumerico);
     
-    //conversão de valores em dólar para real
-    var valorEmDolar = valorEmDolarNumerico.toFixed(1) / 0.18; /* Cotação do dia 14 de setembro 2021 */
-    console.log(valorEmDolar);
-
     var elementoValorConvertido = document.getElementById('valorConvertido');
-    var valorConvertido = 'O resultado em Real é R$' + valorEmDolar;
+    var valorConvertido = 'O resultado em Real é R$' + valorEmReal;
     elementoValorConvertido.innerHTML = valorConvertido;   
-}
+    }
+
+    //convertendo real para dólar
+    function Convertido() {
+    let elementoValor = document.getElementById('valores');
+    let valores = elementoValor.value;
+    let valorEmDolarNumerico = parseInt(valores); 
+        
+    let valorEmDolar = valorEmDolarNumerico.toFixed(2) * 5.00; /* Cotação do dia 14 de setembro 2021 */
+    console.log(valorEmDolar);
+    
+    let elementoMoedaConvertida = document.getElementById('trocandoValores');
+    let trocandoValores = 'O resultado em Dolar é US$' + valorEmDolar;
+    elementoMoedaConvertida.innerHTML = trocandoValores;   
+    }
